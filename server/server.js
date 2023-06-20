906259a263dfd6b904fa6d479d66c31b5ced3275
Ebo Arcadia <yiboli.coding@gmail.com>
2023-06-20 19:42:40 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,40 @@
+const path = require("path");
 const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const buildPath = path.join(__dirname, "..", "build");
 
-const options = {
-  method: "GET",
-  url: "https://jsearch.p.rapidapi.com/search",
-  params: {
-    query: "Python developer in Texas, USA",
-    page: "1",
-    num_pages: "1",
-    remote_jobs_only: "true",
-  },
-  headers: {
-    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-    "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-  },
-};
+app.use(express.static(buildPath));
+app.use(cors());
 
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
+app.get("/jobs", async (req, res) => {
+  try {
+    let {
+      description = "",
+      remote_jobs_only,
+      location = "",
+      page = 1,
+    } = req.query;
+    description = description ? encodeURIComponent(description) : "";
+    location = location ? encodeURIComponent(location) : "";
+    remote_jobs_only =
+      remote_jobs_only === "true" ? "&remote_jobs_only=true" : "";
+
+    if (page) {
+      page = parseInt(page);
+      page = isNaN(page) ? "" : `&page=${page}`;
+    }
+
+    const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${remote_jobs_only}${page}`;
+    const result = await axios.get(query);
+    res.send(result.data);
+  } catch (error) {
+    res.status(400).send("Error while getting list of jobs.Try again later.");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
